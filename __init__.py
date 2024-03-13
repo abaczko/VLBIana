84829b8dfa6d0828292e42b891f24fcbeb61cd86
Anne-Kathrin Baczko <akbaczko@gmail.com>
2024-03-13 10:09:03 +0100

    update

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# VLBIana/__init__.py
+
+""" Organizing, analyzing and plotting of VLBI data.
+
+Modules exported by this package:
+
+- `align_imagesEHTim`: Align two VLBI maps using 2D crosscorrelation.
+- `derive_beta_theta`: Derive and plot allowed parameter space for Theta and Beta.
+- `plot_spix`: Plot the spectral index map.
+- `stackedImages`: Derive and plot a stacked mape out of several VLBI epochs.
+- `cleanMap`: Provides the cleanMap class to plot VLBI clean map and models.
+- `modules/fit_functions`: Helper providing fitting functions for other modules.
+- `modules/jet_calculus`: General functions used in other modules.
+- `modules/plot_functions`: Collection of general plotting functions.
+- `ridgeline/`: Derive ridgeline for straight jets using aips and Python.
+
+Examples for running the modules are in the folder examples.
+
+Example data for running the modules is in examples/example_data.
+"""
